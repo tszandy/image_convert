b57bd239913a19f58a3be806903b37a6d5aeac93
wei xie <444980834@qq.com>
2018-04-25 10:31:48 -0400

    some change

--- test.py
+++ test.py
@@ -4,10 +4,13 @@ import cv2
 import time
 from converter import *
 
+url='dog.png'
+img=cv2.imread(url)
+
 newimg=RBG_to_dilation(img)
 imsave('./images/dog_to_line.png',newimg)
 
-newimg=cv2.imread('dog.png', cv2.IMREAD_GRAYSCALE)
+newimg=cv2.imread(url, cv2.IMREAD_GRAYSCALE)
 imsave('./images/dog_to_grey.png',newimg)
 
 newimg=RBG_to_YUV(img)
@@ -30,11 +33,13 @@ imsave('./images/dog_back_to_color.png',grey_to_RGB+color_to_RGB)
 
 
 
-img=imread('anime.jpg')[:,:,:3].astype('float32')
+url='anime.jpg'
+img=imread(url)[:,:,:3].astype('float32')
+
 newimg=RBG_to_dilation(img)
 imsave('./images/anime_to_line.png',newimg)
 
-newimg=cv2.imread('anime.jpg', cv2.IMREAD_GRAYSCALE)
+newimg=cv2.imread(url, cv2.IMREAD_GRAYSCALE)
 imsave('./images/anime_to_grey.png',newimg)
 
 newimg=RBG_to_YUV(img)
@@ -47,11 +52,12 @@ imsave('./images/anime_UV_to_RGB.png',newimg)
 grey=imread('./images/anime_to_grey.png').astype('float32')
 grey_to_RGB = cv2.cvtColor(grey, cv2.COLOR_GRAY2RGB )
 
+
 UV=RBG_to_YUV(img)
 UV[:,:,0]=0
 color_to_RGB=YUV_to_RGB(UV)
 
 back=grey_to_RGB+color_to_RGB
-img=imread('anime.jpg')[:,:,:3].astype('float32')
+img=imread(url)[:,:,:3].astype('float32')
 
 imsave('./images/anime_back_to_color.png',grey_to_RGB+color_to_RGB)
